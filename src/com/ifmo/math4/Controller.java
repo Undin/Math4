8da7d61c440f108983d6151add1eca3395a4fa20
Arseniy Pendryak <warrior@yandex-team.ru>
2014-10-13 23:15:36 +0400

    renamed package

--- src/com/ifmo/math4/Controller.java
+++ src/com/ifmo/math4/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package com.ifmo.math4;
 
 public class Controller {
 }
--- src/com/ifmo/math4/Main.java
+++ src/com/ifmo/math4/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.ifmo.math4;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
