e17a82435ac289e198b5348a9664b0ff62d02117
Arseniy Pendryak <warrior@yandex-team.ru>
2014-10-13 23:26:31 +0400

    minor modification of AbstractScheme

--- src/com/ifmo/math4/schemes/AbstractScheme.java
+++ src/com/ifmo/math4/schemes/AbstractScheme.java
@@ -11,16 +11,17 @@ public abstract class AbstractScheme {
     protected final double kappa;
     protected final double dx;
     protected final double dt;
-    protected final double[] initialValues;
     protected final Function<Double, Double> leftPoint;
     protected final Function<Double, Double> rightPoint;
+
+    protected double[] previousTimeLayer;
 
     public AbstractScheme(double velocity, double kappa, double dx, double dt, double[] initialValues, Function<Double, Double> leftPoint, Function<Double, Double> rightPoint) {
         this.velocity = velocity;
         this.kappa = kappa;
         this.dx = dx;
         this.dt = dt;
-        this.initialValues = initialValues;
+        this.previousTimeLayer = initialValues;
         this.leftPoint = leftPoint;
         this.rightPoint = rightPoint;
     }
