59f07861f86386fb72337f5790640f1b17f383fb
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-10-15 02:07:44 +0400

    add check box logic

--- src/com/ifmo/math4/Controller.java
+++ src/com/ifmo/math4/Controller.java
@@ -2,10 +2,13 @@ package com.ifmo.math4;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -24,8 +27,28 @@ public class Controller implements Initializable {
     @FXML
     private TextField dxTextView;
 
+    @FXML
+    private CheckBox explicitUpstream;
+    @FXML
+    private CheckBox explicitDownstream;
+    @FXML
+    private CheckBox implicitUpstream;
+    @FXML
+    private CheckBox implicitDownstream;
+    @FXML
+    private CheckBox staggeredGrid;
+
+    private CheckBox[] methods;
+
+    @FXML
+    private Button run;
+    @FXML
+    private Button stop;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        methods = new CheckBox[]{explicitUpstream, explicitDownstream, implicitUpstream, implicitDownstream, staggeredGrid};
     }
 
     private void addPlot(String name, double[] xAxis, double[] yAxis) {
@@ -44,6 +67,49 @@ public class Controller implements Initializable {
 
     private void clearPlot() {
         plot.setData(FXCollections.<XYChart.Series<Double, Double>>observableArrayList());
+    }
+
+    @FXML
+    private void explicitUpstreamChanged(ActionEvent event) {
+        changeCheckBoxesState(explicitUpstream);
+    }
+
+    @FXML
+    private void explicitDownstreamChanged(ActionEvent event) {
+        changeCheckBoxesState(explicitDownstream);
+    }
+
+    @FXML
+    private void implicitUpstreamChanged(ActionEvent event) {
+        changeCheckBoxesState(implicitUpstream);
+    }
+
+    @FXML
+    private void implicitDownstreamChanged(ActionEvent event) {
+        changeCheckBoxesState(implicitDownstream);
+    }
+
+    @FXML
+    private void staggeredGridChanged(ActionEvent event) {
+        changeCheckBoxesState(staggeredGrid);
+    }
+
+    private void changeCheckBoxesState(CheckBox checkBox) {
+        int index = -1;
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].equals(checkBox)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (methods[index].isSelected()) {
+            for (int j = 0; j < methods.length; j++) {
+                if (index != j) {
+                    methods[j].setSelected(false);
+                }
+            }
+        }
     }
 
 }
