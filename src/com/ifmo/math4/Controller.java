aab3d0904d20a7519d3748180c9fc2aea4e2fd37
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-10-15 12:04:40 +0400

    add function choiceBoxes

--- src/com/ifmo/math4/Controller.java
+++ src/com/ifmo/math4/Controller.java
@@ -13,14 +13,21 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.function.Function;
 
 public class Controller implements Initializable {
+
+    private static final long DELAY = 300;
+
+    private Random random = new Random();
 
     @FXML
     private LineChart<Double, Double> plot;
@@ -49,9 +56,18 @@ public class Controller implements Initializable {
     private CheckBox[] methods;
 
     @FXML
-    private Button run;
+    private ChoiceBox<String> initFunctions;
     @FXML
-    private Button stop;
+    private ChoiceBox<String> leftFunctions;
+    @FXML
+    private ChoiceBox<String> rightFunctions;
+
+    @FXML
+    private Button start;
+    @FXML
+    private Button pause;
+    @FXML
+    private Button resume;
 
     private double velocity;
     private double kappa;
@@ -59,30 +75,35 @@ public class Controller implements Initializable {
     private double dt;
     private int number;
 
-    private Timer timer;
+    private Timer timer = new Timer();
+    private AbstractScheme scheme;
+    private double[] x;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         methods = new CheckBox[]{explicitUpstream, explicitDownstream, implicitUpstream, implicitDownstream, staggeredGrid};
+        plot.setCreateSymbols(false);
+        plot.setLegendVisible(false);
+        plot.getStyleClass().add("thick-chart");
     }
 
-    private void setPlot(String name, double[] xAxis, double[] yAxis) {
+    private void setPlot(double[] xAxis, double[] yAxis) {
         if (xAxis.length != yAxis.length) {
             throw new IllegalArgumentException("xAxis and yAxis length is different");
         }
 
         ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList();
-        LineChart.Series<Double, Double> series = new LineChart.Series<Double, Double>();
-        series.setName(name);
+        LineChart.Series<Double, Double> series = new LineChart.Series<>();
         for (int i = 0; i < xAxis.length; i++) {
-            series.getData().add(new XYChart.Data<Double, Double>(xAxis[i], yAxis[i]));
+            series.getData().add(new XYChart.Data<>(xAxis[i], yAxis[i]));
         }
         lineChartData.add(series);
         plot.setData(lineChartData);
     }
 
     @FXML
-    private void runClick(ActionEvent event) throws InterruptedException {
+    private void startClick(ActionEvent event) throws InterruptedException {
+        pauseClick(event);
         int index = -1;
         for (int i = 0; i < methods.length; i++) {
             if (methods[i].isSelected()) {
@@ -92,22 +113,14 @@ public class Controller implements Initializable {
         }
 
         initParams();
-        double[] f = new double[number];
-        double[] x = new double[number];
-        for (int i = 0; i < number; i++) {
-            x[i] = dx * i;
-            if (i < number / 5) {
-                f[i] = 1;
-            }
-        }
+        double[] f = initValues();
 
-        AbstractScheme scheme = null;
         switch (index) {
             case 0:
-                scheme = new ExplicitUpstreamScheme(velocity, kappa, dx, dt, f, a -> 1., b -> 0.);
+                scheme = new ExplicitUpstreamScheme(velocity, kappa, dx, dt, f, getBoundFunction(leftFunctions), getBoundFunction(rightFunctions));
                 break;
             case 1:
-                scheme = new ExplicitDownstreamScheme(velocity, kappa, dx, dt, f, a -> 1., b -> 0.);
+                scheme = new ExplicitDownstreamScheme(velocity, kappa, dx, dt, f, getBoundFunction(leftFunctions), getBoundFunction(rightFunctions));
                 break;
             case 2:
 //                scheme = new ImplicitUpstreamScheme();
@@ -122,25 +135,22 @@ public class Controller implements Initializable {
                 return;
         }
 
-        setPlot("asd", x, f);
-        timer = new Timer();
-        timer.schedule(new Drawer(scheme, x, "asd"), 300, 300);
+        setPlot(x, f);
+        resumeClick(event);
     }
 
     private class Drawer extends TimerTask {
         private AbstractScheme scheme;
         private double[] x;
-        private String name;
 
-        public Drawer(AbstractScheme scheme, double[] x, String name) {
+        public Drawer(AbstractScheme scheme, double[] x) {
             this.scheme = scheme;
             this.x = x;
-            this.name = name;
         }
 
         @Override
         public void run() {
-            Platform.runLater(() -> setPlot(name, x, scheme.nextTimeLayer()));
+            Platform.runLater(() -> setPlot(x, scheme.nextTimeLayer()));
         }
     }
 
@@ -156,10 +166,53 @@ public class Controller implements Initializable {
         }
     }
 
+    private double[] initValues() {
+        Function<Integer, Double> function;
+        switch (initFunctions.getValue()) {
+            case "x < n / 5 ? 1 : 0":
+                function = i -> i < number / 5 ? 1. : 0.;
+                break;
+            case "Delta":
+                function = i -> i == number / 2 ? 100. : 0.;
+                break;
+            case "Random":
+                function = i -> random.nextDouble();
+                break;
+            default:
+                return null;
+        }
+
+        x = new double[number];
+        double[] f = new double[number];
+        for (int i = 0; i < number; i++) {
+            x[i] = dx * i;
+            f[i] = function.apply(i);
+        }
+        return f;
+    }
+
+    private Function<Double, Double> getBoundFunction(ChoiceBox<String> choiceBox) {
+        switch (choiceBox.getValue()) {
+            case "0":
+                return x -> 0.;
+            case "1":
+                return x -> 1.;
+            default:
+                return null;
+        }
+    }
+
     @FXML
-    private void stopClick(ActionEvent event) {
+    private void pauseClick(ActionEvent event) {
         timer.cancel();
-        System.out.println("STOP");
+    }
+
+    @FXML
+    private void resumeClick(ActionEvent event) {
+        timer = new Timer();
+        if (scheme != null) {
+            timer.schedule(new Drawer(scheme, x), 0, DELAY);
+        }
     }
 
     @FXML
@@ -204,5 +257,4 @@ public class Controller implements Initializable {
             }
         }
     }
-
 }
